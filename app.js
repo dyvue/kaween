aae2a5d1996d0a965859d6e0a503f81f49489029
Romadhan Edy Prasetyo <romadhanedy@gmail.com>
2022-06-04 11:32:05 +0700

    Disable(main): Feature

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-var { uuid } = require("uuidv4");
-var db = require("./database.js");
+// var { uuid } = require("uuidv4");
+// var db = require("./database.js");
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 // Server port
 const HTTP_PORT = process.env.PORT || 3000;
@@ -18,51 +18,51 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to kaween!" });
 });
 // Insert here other API endpoints
-app.get("/messages", (req, res, next) => {
-  var sql = "SELECT * FROM messages";
-  var params = [];
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-app.post("/messages", (req, res, next) => {
-  var errors = [];
-  if (!req.body.name) {
-    errors.push("No name specified");
-  }
-  if (!req.body.message) {
-    errors.push("No message specified");
-  }
-  if (errors.length) {
-    res.status(400).json({ error: errors.join(",") });
-    return;
-  }
-  var data = {
-    id: uuid(),
-    name: req.body.name,
-    message: req.body.message,
-  };
-  var sql = "INSERT INTO messages (id, name, message) VALUES (?,?,?)";
-  var params = [data.id, data.name, data.message];
-  db.run(sql, params, function (err, result) {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: data,
-      id: this.lastID,
-    });
-  });
-});
+// app.get("/messages", (req, res, next) => {
+//   var sql = "SELECT * FROM messages";
+//   var params = [];
+//   db.all(sql, params, (err, rows) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: "success",
+//       data: rows,
+//     });
+//   });
+// });
+// app.post("/messages", (req, res, next) => {
+//   var errors = [];
+//   if (!req.body.name) {
+//     errors.push("No name specified");
+//   }
+//   if (!req.body.message) {
+//     errors.push("No message specified");
+//   }
+//   if (errors.length) {
+//     res.status(400).json({ error: errors.join(",") });
+//     return;
+//   }
+//   var data = {
+//     id: uuid(),
+//     name: req.body.name,
+//     message: req.body.message,
+//   };
+//   var sql = "INSERT INTO messages (id, name, message) VALUES (?,?,?)";
+//   var params = [data.id, data.name, data.message];
+//   db.run(sql, params, function (err, result) {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: "success",
+//       data: data,
+//       id: this.lastID,
+//     });
+//   });
+// });
 
 // Default response for any other request
 app.use(function (req, res) {
