b9de089f8ba68d4260fafc0cd45671fbefa8a110
Romadhan Edy Prasetyo <romadhanedy@gmail.com>
2022-06-04 12:34:32 +0700

    Add(feature): Guest and messages

--- routes/routes.js
+++ routes/routes.js
@@ -1,73 +1,69 @@
-const express = require('express');
-const Model = require('../models/model');
+const express = require("express");
+const MGuest = require("../models/guest");
+const MMessage = require("../models/message");
 const router = express.Router();
 
-//Post Method
-router.post('/post', async (req, res) => {
-    const data = new Model({
-        name: req.body.name,
-        age: req.body.age
-    })
+// Guests
+router.post("/guests", async (req, res) => {
+  const data = new MGuest({
+    name: req.body.name
+  });
 
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+router.get("/guests", async (req, res) => {
+  try {
+    const data = await MGuest.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.delete("/guests/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await MGuest.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-//Get all Method
-router.get('/getAll', async (req, res) => {
-    try {
-        const data = await Model.find();
-        res.json(data)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+// Messages
+router.post("/messages", async (req, res) => {
+  const data = new MMessage({
+    name: req.body.name,
+    message: req.body.message,
+  });
 
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try {
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+router.get("/messages", async (req, res) => {
+  try {
+    const data = await MMessage.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.delete("/messages/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await MMessage.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
